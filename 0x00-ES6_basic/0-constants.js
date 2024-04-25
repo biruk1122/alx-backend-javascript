5303044c63f24e6eead79a135a69bd03112b54d0
biruk1122 <birukfikadu29@gmail.com>
2024-04-25 08:27:14 -0700

    updated

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-  const task = 'I prefer const when I can.';
+  var task = 'I prefer const when I can.';
   return task;
 }
 
@@ -8,7 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let';
+  var combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
